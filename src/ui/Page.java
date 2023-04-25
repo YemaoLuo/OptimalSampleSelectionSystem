69953a20d734ec62dc55ee248cb1b6e893e82f23
LuoYemao <leafysn@qq.com>
2023-04-25 15:18:48 +0800

    basic ui

--- src/ui/Page.java
+++ src/ui/Page.java
@@ -1,11 +1,10 @@
 package ui;
 
-import algorithm.SolutionHelper1_1;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -14,99 +13,134 @@ public class Page extends JFrame {
     private JTextField textField1, textField2, textField3, textField4, textField5;
     private JButton button;
     private JTextArea textArea;
+    private JProgressBar progressBar;
+
 
     public Page() {
         setTitle("Optimize samples system");
-        setSize(600, 400);
+        setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(6, 2));
+        panel.setLayout(new GridLayout(8, 2));
 
         label1 = new JLabel("m：");
-        panel.add(label1);
         textField1 = new JTextField();
+        panel.add(label1);
         panel.add(textField1);
 
         label2 = new JLabel("n：");
-        panel.add(label2);
         textField2 = new JTextField();
+        panel.add(label2);
         panel.add(textField2);
 
         label3 = new JLabel("k：");
-        panel.add(label3);
         textField3 = new JTextField();
+        panel.add(label3);
         panel.add(textField3);
 
         label4 = new JLabel("j：");
-        panel.add(label4);
         textField4 = new JTextField();
+        panel.add(label4);
         panel.add(textField4);
 
         label5 = new JLabel("s：");
-        panel.add(label5);
         textField5 = new JTextField();
+        panel.add(label5);
         panel.add(textField5);
+
+        progressBar = new JProgressBar(0, 100);
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
+        panel.add(progressBar);
+
+        textArea = new JTextArea();
+        panel.add(textArea);
 
         button = new JButton("开始");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int m = Integer.parseInt(textField1.getText());
-                int n = Integer.parseInt(textField2.getText());
-                int k = Integer.parseInt(textField3.getText());
-                int j = Integer.parseInt(textField4.getText());
-                int s = Integer.parseInt(textField5.getText());
-
-                SolutionHelper1_1 sh = new SolutionHelper1_1();
-                long startTime = System.currentTimeMillis();
-                System.out.println("=====================================");
-
-                long tempTime = System.currentTimeMillis();
-                java.util.List<Integer> chosenSamples = sh.generateChosenSamples(m, n);
-                System.out.println("Chosen samples: " + chosenSamples);
-                System.out.println("Chosen samples size: " + chosenSamples.size());
-                System.out.println("Time cost: " + (System.currentTimeMillis() - tempTime) + " ms");
-                System.out.println("=====================================");
-
-                tempTime = System.currentTimeMillis();
-                java.util.List<java.util.List<Integer>> possibleResults = sh.generatePossibleResults(chosenSamples, k);
-                //System.out.println("Possible results: " + possibleResults);
-                System.out.println("Possible results size: " + possibleResults.size());
-                System.out.println("Time cost: " + (System.currentTimeMillis() - tempTime) + " ms");
-                System.out.println("=====================================");
-
-                tempTime = System.currentTimeMillis();
-                java.util.List<java.util.List<Integer>> coverList = sh.generateCoverList(chosenSamples, j);
-                Map<java.util.List<Integer>, java.util.List<java.util.List<Integer>>> coverListMap = sh.generateCoverListMap(coverList, s);
-                //System.out.println("Cover list map: " + coverListMap);
-                System.out.println("Cover list map size: " + coverListMap.size());
-                System.out.println("Time cost: " + (System.currentTimeMillis() - tempTime) + " ms");
-                System.out.println("=====================================");
-
-                tempTime = System.currentTimeMillis();
-                List<List<Integer>> result = sh.getResult(possibleResults, coverListMap);
-                System.out.println("Result: " + result);
-                System.out.println("Result size: " + result.size());
-                System.out.println("Time cost: " + (System.currentTimeMillis() - tempTime) + " ms");
-                System.out.println("=====================================");
-
-                System.out.println("Total time cost: " + (System.currentTimeMillis() - startTime) + " ms");
-                System.out.println("=====================================");
-
-                textArea.setText("Result: " + result + "\n"
-                        + "Reuslt Size: " + result.size() + "\n"
-                        + "Total time cost: " + (System.currentTimeMillis() - startTime) + " ms\n");
+                process();
             }
         });
         panel.add(button);
 
-        textArea = new JTextArea();
-        panel.add(textArea);
-
         add(panel);
 
         setVisible(true);
+    }
+
+    public void process() {
+        int m = Integer.parseInt(textField1.getText());
+        int n = Integer.parseInt(textField2.getText());
+        int k = Integer.parseInt(textField3.getText());
+        int j = Integer.parseInt(textField4.getText());
+        int s = Integer.parseInt(textField5.getText());
+
+        SolutionHelperUI sh = new SolutionHelperUI();
+        long startTime = System.currentTimeMillis();
+        System.out.println("=====================================");
+
+        long tempTime = System.currentTimeMillis();
+        java.util.List<Integer> chosenSamples = sh.generateChosenSamples(m, n);
+        System.out.println("Chosen samples: " + chosenSamples);
+        System.out.println("Chosen samples size: " + chosenSamples.size());
+        System.out.println("Time cost: " + (System.currentTimeMillis() - tempTime) + " ms");
+        System.out.println("=====================================");
+
+        tempTime = System.currentTimeMillis();
+        java.util.List<java.util.List<Integer>> possibleResults = sh.generatePossibleResults(chosenSamples, k);
+        //System.out.println("Possible results: " + possibleResults);
+        System.out.println("Possible results size: " + possibleResults.size());
+        System.out.println("Time cost: " + (System.currentTimeMillis() - tempTime) + " ms");
+        System.out.println("=====================================");
+
+        tempTime = System.currentTimeMillis();
+        java.util.List<java.util.List<Integer>> coverList = sh.generateCoverList(chosenSamples, j);
+        Map<java.util.List<Integer>, java.util.List<java.util.List<Integer>>> coverListMap = sh.generateCoverListMap(coverList, s);
+        //System.out.println("Cover list map: " + coverListMap);
+        System.out.println("Cover list map size: " + coverListMap.size());
+        System.out.println("Time cost: " + (System.currentTimeMillis() - tempTime) + " ms");
+        System.out.println("=====================================");
+
+        tempTime = System.currentTimeMillis();
+
+        List<List<Integer>> result = new ArrayList<>();
+        double initSize = coverListMap.size();
+        long removeCoverListMapKeyTime = 0, getCandidateResultTime = 0;
+        double percent = 0;
+        while (!coverListMap.isEmpty()) {
+            percent = (1 - coverListMap.size() / initSize) * 100;
+            progressBar.setValue((int) percent);
+            progressBar.repaint();
+            long startTime5 = System.currentTimeMillis();
+            System.out.println(String.format("%.2f", (1 - coverListMap.size() / initSize) * 100) + "%");
+            List<Integer> candidateResult = sh.getCandidateResult(possibleResults, coverListMap);
+            getCandidateResultTime += System.currentTimeMillis() - startTime5;
+            startTime5 = System.currentTimeMillis();
+            result.add(candidateResult);
+            sh.removeCoverListMapKey(candidateResult, coverListMap);
+            possibleResults.remove(candidateResult);
+            removeCoverListMapKeyTime += System.currentTimeMillis() - startTime5;
+        }
+        System.out.println("Remove cover list time: " + removeCoverListMapKeyTime + " ms");
+        System.out.println("Get candidate result time: " + getCandidateResultTime + " ms");
+        System.out.println("=========================================");
+
+        System.out.println("Result: " + result);
+        System.out.println("Result size: " + result.size());
+        System.out.println("Time cost: " + (System.currentTimeMillis() - tempTime) + " ms");
+        System.out.println("=====================================");
+
+        System.out.println("Total time cost: " + (System.currentTimeMillis() - startTime) + " ms");
+        System.out.println("=====================================");
+
+        progressBar.setValue(100);
+
+        textArea.setText("Result: " + result + "\n"
+                + "Reuslt Size: " + result.size() + "\n"
+                + "Total time cost: " + (System.currentTimeMillis() - startTime) + " ms\n");
     }
 
     public static void main(String[] args) {
---
+++ src/ui/SolutionHelperUI.java
@@ -0,0 +1,130 @@
+package ui;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SolutionHelperUI {
+
+    // Generate chosen samples
+    public List<Integer> generateChosenSamples(int m, int n) {
+        List<Integer> chosenSamples = new ArrayList<>();
+        Random random = new Random();
+        while (chosenSamples.size() < n) {
+            int candidateNum = random.nextInt(m) + 1;
+            if (chosenSamples.contains(candidateNum)) {
+                continue;
+            }
+            chosenSamples.add(candidateNum);
+        }
+        return chosenSamples;
+    }
+
+
+    // Generate all possible combinations of results
+    public List<List<Integer>> generatePossibleResults(List<Integer> chosenSamples, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        generateResultsCombinations(chosenSamples, k, 0, temp, result);
+        return result;
+    }
+
+    private static void generateResultsCombinations(List<Integer> chosenSamples, int k, int start,
+                                                    List<Integer> temp, List<List<Integer>> result) {
+        if (temp.size() == k) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = start; i < chosenSamples.size(); i++) {
+            temp.add(chosenSamples.get(i));
+            generateResultsCombinations(chosenSamples, k, i + 1, temp, result);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
+
+    // Generate all results need to be covered
+    public List<List<Integer>> generateCoverList(List<Integer> chosenSamples, int j) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        generateCoverListCombinations(chosenSamples, j, 0, temp, result);
+        return result;
+    }
+
+    private static void generateCoverListCombinations(List<Integer> chosenSamples, int k, int start,
+                                                      List<Integer> temp, List<List<Integer>> result) {
+        if (temp.size() == k) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = start; i < chosenSamples.size(); i++) {
+            temp.add(chosenSamples.get(i));
+            generateCoverListCombinations(chosenSamples, k, i + 1, temp, result);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
+    public Map<List<Integer>, List<List<Integer>>> generateCoverListMap(List<List<Integer>> coverList, int s) {
+        Map<List<Integer>, List<List<Integer>>> coverListMap = new HashMap<>((int) (coverList.size() / 0.75 + 1));
+        for (List<Integer> integers : coverList) {
+            coverListMap.put(integers, generateCoverList(integers, s));
+        }
+        return coverListMap;
+    }
+
+
+    // Generate results using hill-climbing method
+    public void removeCoverListMapKey(List<Integer> candidateResult, Map<List<Integer>, List<List<Integer>>> coverListMap) {
+        coverListMap.remove(candidateResult);
+        Iterator<Map.Entry<List<Integer>, List<List<Integer>>>> iterator = coverListMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<List<Integer>, List<List<Integer>>> next = iterator.next();
+            List<List<Integer>> coverList = next.getValue();
+            for (List<Integer> integerList : coverList) {
+                if (candidateResult.containsAll(integerList)) {
+                    iterator.remove();
+                    break;
+                }
+            }
+        }
+    }
+
+    public List<Integer> getCandidateResult(List<List<Integer>> possibleResults, Map<List<Integer>, List<List<Integer>>> coverListMap) {
+        Map<List<Integer>, Integer> countMap = new ConcurrentHashMap<>((int) (possibleResults.size() / 0.75 + 1));
+        possibleResults.parallelStream().forEach(possibleResult -> {
+            coverListMap.entrySet().parallelStream().forEach(next -> {
+                List<List<Integer>> coverList = next.getValue();
+                for (List<Integer> integerList : coverList) {
+                    if (possibleResult.containsAll(integerList)) {
+                        countMap.merge(possibleResult, 1, Integer::sum);
+                        break;
+                    }
+                }
+            });
+        });
+        Optional<Map.Entry<List<Integer>, Integer>> maxEntry = countMap.entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue());
+        return maxEntry.map(Map.Entry::getKey).orElse(null);
+    }
+
+    public List<List<Integer>> getResult(List<List<Integer>> possibleResults, Map<List<Integer>, List<List<Integer>>> coverListMap) {
+        List<List<Integer>> result = new ArrayList<>();
+        double initSize = coverListMap.size();
+        long removeCoverListMapKeyTime = 0, getCandidateResultTime = 0;
+        while (!coverListMap.isEmpty()) {
+            long startTime = System.currentTimeMillis();
+            System.out.println(String.format("%.2f", (1 - coverListMap.size() / initSize) * 100) + "%");
+            List<Integer> candidateResult = getCandidateResult(possibleResults, coverListMap);
+            getCandidateResultTime += System.currentTimeMillis() - startTime;
+            startTime = System.currentTimeMillis();
+            result.add(candidateResult);
+            removeCoverListMapKey(candidateResult, coverListMap);
+            possibleResults.remove(candidateResult);
+            removeCoverListMapKeyTime += System.currentTimeMillis() - startTime;
+        }
+        System.out.println("Remove cover list time: " + removeCoverListMapKeyTime + " ms");
+        System.out.println("Get candidate result time: " + getCandidateResultTime + " ms");
+        System.out.println("=========================================");
+        return result;
+    }
+}
